344b2bb763135a310a2d0b9cabf3fa0fee51df48
kgauzampe <kgauza.mpe@gmail.com>
2021-01-15 13:54:05 +0200

    files added

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+const api = {
+  key: "dfa6938a33cd0707cbf42005968a7dac",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric$APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setQuery("");
+          setWeather(result);
+          console.log(result);
+        });
+    }
+  };
+
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+  return (
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="searc h-bar"
+            placeholder="Search... "
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+          />
+        </div>
+        <div className="location-box">
+          <div className="location">Johannesburg, South Africa</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">15C</div>
+          <div className="weather">Cloudy</div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
